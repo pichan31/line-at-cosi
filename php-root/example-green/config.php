8b9edb0b0d024fd1fcc915c7b0c33c0813dd2a10
pichan31 <37392081+pichan31@users.noreply.github.com>
2018-05-23 14:46:57 +0700

    Update config.php

--- php-root/example-green/config.php
+++ php-root/example-green/config.php
@@ -5,6 +5,11 @@
 // $access_token  = '+FPbwVPcxPAUeI9hWgihaa+SbMxPrqFCF8l9ljKQuVLKwj7NwiG5lPrj3Hzbk6SD33zAdxi+nnbhtznPtX+8M+MeDrvJ3+5yS1uF6xATQ3mvCZs/+7K6ysy+9Mxqjp+YtkGdqSryBMNdJd2tkmNX+gdB04t89/1O/w1cDnyilFU=';
 
 // green 
-$channelSecret = '284c868918e2a89b8e9d311c8f7ccdf3';
-$access_token  = 'bkL15evcI4ilmJveNzTW1KAUi5Wuir6z62Rj+cW6YKQmrJUVUGXbhwHdYQJRY7GHuSO8oTKr2v6P1mR3rFm0Wqs9DsO0tmamkbQgCw4gnXmQwmZ/CRCxGV4wgoKzZszjQI/pn2Yr+E9NB3ytWEYMIAdB04t89/1O/w1cDnyilFU=';
+// $channelSecret = '284c868918e2a89b8e9d311c8f7ccdf3';
+// $access_token  = 'bkL15evcI4ilmJveNzTW1KAUi5Wuir6z62Rj+cW6YKQmrJUVUGXbhwHdYQJRY7GHuSO8oTKr2v6P1mR3rFm0Wqs9DsO0tmamkbQgCw4gnXmQwmZ/CRCxGV4wgoKzZszjQI/pn2Yr+E9NB3ytWEYMIAdB04t89/1O/w1cDnyilFU=';
+
+
+// cosi 
+$channelSecret = '39abe5e32d8080f0763dc2d5250ba37b';
+$access_token  = 'ctudDt6K/vaMQ0OWQiZOgw16m/0kiIgiMd1WhsI4eEylm52zhgKVIR4QRTfzWIi4qblmcjidTi9chBaNX6Y6iaUxBE5bHKnmm0akixV6aNwX+AfqpvuvTxyi4O1PXuEmrRhm6Ud9vrrJm9+849E9nwdB04t89/1O/w1cDnyilFU=';
 ?>
